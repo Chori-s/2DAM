b2f795a67f7bc06aa1e9d9752a5a2a7cc87926dc
Iván <ilinveg499@g.educaand.es>
2025-11-21 12:40:14 +0100

    21-11-25

---
+++ AccesoADatos/Hibernate/gamevault V2/game-vault/src/main/java/es/dam/gamevault/App.java
@@ -0,0 +1,13 @@
+package es.dam.gamevault;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
